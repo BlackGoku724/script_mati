628424fdd628c89d9eb8b3eb581b1ee08351e2f6
luci <MatiasIbarra222@gmail.com>
2022-03-11 22:09:49 -0300

    chore(scripts): Resolve PR request

--- scripts/script_2.py
+++ scripts/script_2.py
@@ -1,4 +1,10 @@
 #hello world
 
-def FUNCION1():
-    	print('hello world')
+def hello_world(name):
+    try:
+        name= input('ingrese su nombre')
+        print(f'hello world {name}')
+        return 1
+    except Exception as e:
+        print(f'error: {e}')
+        return 0
